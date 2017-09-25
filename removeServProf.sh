3409784df09e9a3ab57d0167ae61d60965e503c6
mpolski <polski@hpe.com>
2017-09-25 14:37:53 +0200

    Welcome to GuruClub

---
+++ removeServProf.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#chck env varialbes for the first three variables
+SERVER=$1
+USER=$2
+PASS=$3
+JSONFILE=$4
+AUTH=$5
+URL="https://$SERVER"
+
+EXTPROFNAME=`cat "$JSONFILE" | jq -r .name`
+
+#echo $SERVER $USER $PASS $JSONFILE
+echo " "
+#echo $AUTH
+echo ""
+echo "Destroying server "$EXTPROFNAME
+curl -s -k -H "X-API-Version: 300" -H "Content-Type: application/json" -H "Auth: $AUTH" -X DELETE $URL/rest/server-profiles?filter=name="'$EXTPROFNAME'"
